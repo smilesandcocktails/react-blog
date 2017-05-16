98df127e3df0895d940916044ff1e4d45c106c57
Cara Chew <carachew@gmail.com>
2017-05-16 15:07:18 +0800

    included like and dislike button to todolist

---
+++ src/Todo.js
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+// import './Todo.css';
+
+
+class Todo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: props.todo.title,
+      list: props.todo.list,
+    }
+  }
+
+  newTodo(e) {
+    e.preventDefault()
+
+    var form = e.target
+    var input = form.querySelector('input')
+    var list = this.state.list
+    list.push({todo: input.value, vote: 0})
+    this.setState ({
+      list: list
+    })
+  }
+
+  resetList(e) {
+    e.preventDefault()
+
+    this.setState ({
+      list: []
+    })
+  }
+
+  like(e) {
+    var list = this.state.list
+    var index = e.target.value
+    var selected = list[index]
+    selected.vote += 1
+    list.splice(index, 1, selected)
+    this.setState ({
+      list: list
+    })
+  }
+
+  dislike(e) {
+    var list = this.state.list
+    var index = e.target.value
+    var selected = list[index]
+    console.log(selected)
+    selected.vote -= 1
+
+    list.splice(index, 1, selected)
+
+    this.setState ({
+      list: list
+    })
+  }
+
+  render() {
+
+    let todoList = this.state.list.map((list, index) => {
+      return<div>
+        <p key={index}>{list.todo} - {list.vote}</p>
+        <button onClick={(e) => this.like(e)} value={index}>Like</button>
+        <button onClick={(e) => this.dislike(e)} value={index}>Dislike</button>
+      </div>
+    })
+
+    return (
+      <div className = "Todo">
+        <div>
+          <h1>{this.state.title}</h1>
+        </div>
+        <div>
+          <h3>List</h3>
+            {todoList}
+        </div>
+        <div>
+          <form onSubmit={(e) => this.newTodo(e)}>
+            <label>
+              New Todo:
+              <input type="text"></input>
+            </label>
+            <button>Submit</button>
+          </form>
+        </div>
+        <div>
+          <button onClick={(e) => this.resetList(e)}>Finished the List!</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Todo;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Post from './Post';
+// import Post from './Post';
+import Todo from './Todo';
 import './index.css';
 
-const post = {
-  title: "Dinosaurs are awesome",
-  author: "Stealthy Stegosaurus",
-  body: "Check this body property!",
-  comments: ["First!", "Great Post", "Hire this author now!"]
+// const post = {
+//   title: "Dinosaurs are awesome",
+//   author: "Stealthy Stegosaurus",
+//   body: "Check this body property!",
+//   comments: ["First!", "Great Post", "Hire this author now!"]
+// }
+
+const todo = {
+  title: 'Things I should stop procrastinating',
+  list: [
+    { todo: 'Buy ice cream',
+      vote: 2},
+    { todo: 'Eat ice cream',
+      vote: 5},
+    { todo: 'Go to the gym',
+      vote: 1}
+    ]
 }
 
 ReactDOM.render(
-  <Post post={post} />,
+  // <Post post={post} />,
+  <Todo todo={todo} />,
   document.getElementById('root')
 );
